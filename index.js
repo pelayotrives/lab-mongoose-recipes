4adcbbe30642d0cd8d727c5f31101cda59c238bf
Pelayo Trives <pelayotrives@gmail.com>
2022-05-05 18:32:09 +0200

    Método async-await añadidos.

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI)
   .then(x => {
     console.log(`Conectado a la BBDD: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
@@ -59,3 +58,50 @@ mongoose
   .catch( (error) => {
     console.error('Error connecting to the database', error);
   });
+
+  //! -----------
+  //! Cómo se haría con async - await y conseguiríamos lo mismo.
+
+  /* async function recipes() {
+    try {
+      await mongoose.connect(MONGODB_URI)
+      console.log(`Conectado a la BBDD.`);
+      console.log("First promise in process.");
+      await Recipe.deleteMany()
+      await Recipe.create({
+        title: "Apple Pie",
+        level: "Easy Peasy",
+        ingredients: ["Flour", "Butter", "Apples", "Sugar", "Eggs", "Cinnammon", "Nutmeg"],
+        cuisine: "German",
+        dishType: "dessert",
+        image: "http://imgfz.com/i/Wd4gMEp.jpeg",
+        duration: 45,
+        creator: "Sarah Jane Calvo"
+      })
+      console.log("Recipe added!")
+      console.log("Second promise in process.")
+      let response = await Recipe.insertMany(data);
+      console.log("Recipes (array) added!")
+      console.log("Third promise in process.")
+      response.forEach( (element) => {
+        console.log(element.title)
+      })
+      console.log("Titles printed!")
+      console.log("Fourth promise in process.")
+      await Recipe.findOneAndUpdate( {title: "Rigatoni alla Genovese"}, { duration: 100 } )
+      console.log("Success!")
+      console.log("Duration updated!")
+      console.log("Fifth promise in process.")
+      await Recipe.deleteOne( {title: "Carrot Cake"}  )
+      console.log("Success!")
+      console.log("Sixth promise in process.")
+      await mongoose.connection.close()
+    }
+
+    catch(err) {
+      console.log("Error!")
+    }
+  } */
+
+
+
